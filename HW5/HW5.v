dcb2289ee09fe1b5f7119fd78f234d9645c4efad
averyizatt <alizatt@mavs.coloradomesa.edu>
2025-09-25 00:01:42 -0600

    HW5

--- HW5/HW5.v
+++ HW5/HW5.v
@@ -1,29 +1,148 @@
-// Code your design here
+`timescale 1ns/1ps
+`default_nettype none
 
 
-module MinTerm(w, x, y, z, f);
+// module BehaveMinTerm(w, x, y, z, f);
+//   input w;
+//   input x;
+//   input y;
+//   input z;
+//   output f;
+  
+//   assign f = (~w & x & y & ~z) | (w & x & ~y &z) | (~x & y & z);
+  
+    
+// endmodule
+
+
+// module BehaveMaxTerm(w, x, y, z, f);
+//   input w;
+//   input x;
+//   input y;
+//   input z;
+//   output f;
+  
+//   assign f = (y | z) & (x | y) & (x | z) & (~w | ~x | ~y) & (w | ~x | ~z);
+  
+    
+// endmodule
+
+// module StructMinTerm(w, x, y, z, f);
+//   input w;
+//   input x;
+//   input y;
+//   input z;
+//   output f;
+  
+//   wire notW, notY, notZ, notX;
+//   wire and1, and2, and3;
+  
+//   not w1(notW, w);
+//   not y1(notY, y);
+//   not z1(notZ, z);
+//   not x1(notX, x);
+  
+//   and a1(and1, notW, x, y, notZ);
+//   and b1(and2, w, x, notY, z);
+//   and c1(and3, notX, z, y);
+  
+//   or or1(f, and1, and2, and3);
+  
+// endmodule
+  
+
+// module StructMaxTerm(w, x, y, z, node1);
+//   input w;
+//   input x;
+//   input y;
+//   input z;
+//   output node1;
+ 
+  
+//   wire notW, notX, notY, notZ;
+//   wire m1, m2, m3, m4, m5;
+  
+//   not nw1(notW, w);
+//   not ny1(notY, y);
+//   not nz1(notZ, z);
+//   not nx1(notX, x);
+  
+  
+//   or or1(m1, x, y);
+//   or or2(m2, y, z);
+//   or or3(m3, x, z);
+//   or or4(m4, notY, notW, notX);
+//   or or5(m5, notZ, notX, w);
+  
+//   and g_and(node1, m1, m2, m3, m4, m5);
+  
+// endmodule
+  
+
+// module TwoInputStructMaxTerm(w, x, y, z, node2);
+//   input w;
+//   input x;
+//   input y;
+//   input z;
+//   output node2;
+ 
+  
+//   wire notW, notX, notY, notZ;
+//   wire m1, m2, m3, m4, m5, m6, m7;
+//   wire a1_and, b1_and, c1_and;
+  
+//   not nw1(notW, w);
+//   not ny1(notY, y);
+//   not nz1(notZ, z);
+//   not nx1(notX, x);
+  
+  
+//   or or1(m1, x, y);
+//   or or2(m2, y, z);
+//   or or3(m3, x, z);
+//   or or4(m4, notX, notW);
+//   or or5(m5, m4, notY);
+//   or or6(m6, w, notX);
+//   or or7(m7, notZ, m6);
+  
+//   and a_and(a1_and, m1, m2);
+//   and b_and(b1_and, m3, a1_and);
+//   and c_and(c1_and, m5, b1_and);
+//   and d_and(node2, m7, c1_and);
+  
+// endmodule
+  
+
+module Test1DelayedTwoInputStructMaxTerm(w, x, y, z, node2);
   input w;
   input x;
   input y;
   input z;
-  output f;
+  output node2;
+ 
   
-  assign f = (~w & x & y & ~z) | (w & x & ~y &z) | (~x & y & z);
+  wire notW, notX, notY, notZ;
+  wire m1, m2, m3, m4, m5, m6, m7;
+  wire a1_and, b1_and, c1_and;
   
-    
+  not nw1(notW, w);
+  not ny1(notY, y);
+  not nz1(notZ, z);
+  not nx1(notX, x);
+  
+  
+  or #2 or1(m1, x, y);
+  or #2 or2(m2, y, z);
+  or #2 or3(m3, x, z);
+  or #2 or4(m4, notX, notW);
+  or #2 or5(m5, m4, notY);
+  or #2 or6(m6, w, notX);
+  or #2 or7(m7, notZ, m6);
+  
+  and #2 a_and(a1_and, m1, m2);
+  and #2 b_and(b1_and, m3, a1_and);
+  and #2 c_and(c1_and, m5, b1_and);
+  and #2 d_and(node2, m7, c1_and);
+  
 endmodule
-
-
-
-
-module MaxTerm(w, x, y, z, f);
-  input w;
-  input x;
-  input y;
-  input z;
-  output f;
-  
-  assign f = (~w & x & y & ~z) | (w & x & ~y &z) | (~x & y & z);
-  
-    
-endmodule
+ 
--- HW5/HW5TestBench.v
+++ HW5/HW5TestBench.v
@@ -1,39 +1,67 @@
-// Code your testbench here
-// or browse Examples
-// Code your testbench here
-// or browse Examples
-module MinMaxTestBench;
-  reg clk;
-  
-  reg w;
-  reg x;
-  reg y;
-  reg z;
- 
-  wire fMin, fMax;
-  
-  integer i;
-  
+`default_nettype none
+`timescale 1ns/1ps
+
+// module BehaveMinMaxTestBench;
+//   reg clk;
+//   reg w;
+//   reg x;
+//   reg y;
+//   reg z;
+//   wire fMin, fMax;
+//   integer i;
     
- MinTerm MUT1 (.w(w), .x(x), .y(y), .z(z), .f(fMin));
- MaxTerm MUT2 (.w(w), .x(x), .y(y), .z(z), .f(fMax));
+//   BehaveMinTerm MUT1 (.w(w), .x(x), .y(y), .z(z), .f(fMin));
+//   BehaveMaxTerm MUT2 (.w(w), .x(x), .y(y), .z(z), .f(fMax));
   
   
   
-  initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars(1, MinMaxTestBench);
-    clk = 1;
+//  initial begin
+//     $dumpfile("dump.vcd");
+//    $dumpvars(1, BehaveMinMaxTestBench);
+//     clk = 1;
+//     for (i = 0; i < 16; i = i + 1)
+//       begin
+//       {w, x, y, z} = i;
+//       #1;
+//    	  $display( w, x, y, z, fMin, fMax);
+//              end
+//         $finish;
     
-    
-    for (i = 0; i < 16; i = i + 1) begin
-      {w, x, y, z} = i;
-    		#1;
-    $display( w, x, y, z, fMin, fMax);
-             end
-        $finish;
-    
-  end
+//   end
 
   
+// endmodule
+
+
+//   StructMinTerm MUT1 (.w(w), .x(x), .y(y), .z(z), .f(fMin));
+//   StructMaxTerm MUT2 (.w(w), .x(x), .y(y), .z(z), .node1(fMax));
+//   TwoInputStructMaxTerm MUT3 (.w(w), .x(x), .y(y), .z(z), .node2(fMax));
+//    $display( w, x, y, z, fMin);
+
+
+module TestDelayedTwoInput;
+  reg clk;
+  reg w = 0;
+  reg x = 0;
+  reg y = 0;
+  reg z = 0;
+  wire fMin, fMax;
+  integer i = 0;
+  Test1DelayedTwoInputStructMaxTerm MUT3 (.w(w), .x(x), .y(y), .z(z), .node2(fMax)); 
+  always #2 clk = ~clk;
+  
+ initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, TestDelayedTwoInput);
+    clk = 1;
+    i = 0;
+   
+    for (i = 0; i < 16; i = i + 1)
+      begin
+      {w, x, y, z} = i;
+      #12;
+   	  $display( w, x, y, z, fMax);
+             end
+        #70 $finish;
+ end
 endmodule
